9c1876b2bc25e8d922d602701b890065f759a2e9
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2022-09-11 21:57:30 -0300

    Improve Main Window

---
+++ src/GroupFacesWindow.py
@@ -0,0 +1,29 @@
+from tkinter import Image
+from PySide6 import QtGui, QtCore, QtWidgets
+from .Image import Image
+
+
+class GroupFacesWindow(QtWidgets.QWidget):
+    def __init__(self, images: list[Image]) -> None:
+        super().__init__()
+
+        self._images = images
+
+        self._gridSize = QtCore.QSize(150, 150)
+        self._iconSize = QtCore.QSize(130, 100)
+        self._listWidget = QtWidgets.QListWidget()
+        self._listWidget.setContentsMargins(0, 0, 0, 0)
+        self._listWidget.setGridSize(self._gridSize)
+        self._listWidget.setIconSize(self._iconSize)
+        self._listWidget.setViewMode(QtWidgets.QListView.ViewMode.IconMode)
+        self._listWidget.setFlow(QtWidgets.QListView.Flow.LeftToRight)
+        self._listWidget.setResizeMode(QtWidgets.QListView.ResizeMode.Adjust)
+        self._listWidget.setMovement(QtWidgets.QListView.Movement.Static)
+        self._listWidget.setDragDropMode(QtWidgets.QListView.DragDropMode.NoDragDrop)
+        self._listWidget.setSelectionMode(QtWidgets.QListView.SelectionMode.ExtendedSelection)
+        self._listWidget.setSelectionBehavior(QtWidgets.QListView.SelectionBehavior.SelectItems)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self._listWidget)
+        self.setLayout(layout)
--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,4 +1,4 @@
-from PySide6 import QtCore, QtWidgets
+from PySide6 import QtGui, QtCore, QtWidgets
 
 from .QtHelpers import setSplitterStyle
 from .Widgets.ImageGrid import ImageGrid
@@ -6,9 +6,7 @@ from .Widgets.InspectorPanel import InspectorPanel
 from .Image import Image
 from .PerspectiveWindow import PerspectiveWindow
 from .DeblurWindow import DeblurWindow
-from phantom.utils import draw_faces
-from phantom.faces import landmark
-import cv2
+from .GroupFacesWindow import GroupFacesWindow
 import glob
 
 
@@ -28,17 +26,43 @@ class MainWindow(QtWidgets.QMainWindow):
         self._layout.setContentsMargins(0, 0, 0, 0)
         self._layout.setSpacing(0)
 
-        self.cam_detect_demo_button = QtWidgets.QPushButton("Open Cam detect demo")
-        self.cam_detect_demo_button.clicked.connect(self.open_cam_detect_demo)
-        self._layout.addWidget(self.cam_detect_demo_button, 0, QtCore.Qt.AlignTop)
+        self._addImagesAction = QtGui.QAction(
+            QtGui.QIcon("res/image_add.png"), "Add Images", self)
+        self._addImagesAction.setToolTip("Add images to current project")
+        self._addImagesAction.triggered.connect(self.onAddImagesPressed)
 
-        self.perspective_correct_button = QtWidgets.QPushButton("Open Perspective Correct Window")
-        self.perspective_correct_button.clicked.connect(self.open_perspective_correct_window)
-        self._layout.addWidget(self.perspective_correct_button, 0, QtCore.Qt.AlignTop)
+        self._exportImageAction = QtGui.QAction(
+            QtGui.QIcon("res/image_save.png"), "Export Image", self)
+        self._exportImageAction.triggered.connect(self.onExportImagePressed)
 
-        self.deblur_button = QtWidgets.QPushButton("Open Deblur Window")
-        self.deblur_button.clicked.connect(self.open_deblur_window)
-        self._layout.addWidget(self.deblur_button, 0, QtCore.Qt.AlignTop)
+        self._correctPerspectiveAction = QtGui.QAction(
+            QtGui.QIcon("res/correct_perspective.png"), "Correct Perspective", self)
+        self._correctPerspectiveAction.triggered.connect(self.onCorrectPerspectivePressed)
+
+        self._deblurAction = QtGui.QAction(
+            QtGui.QIcon("res/deblur.png"), "Deblur Filter", self)
+        self._deblurAction.triggered.connect(self.onDeblurPressed)
+
+        self._groupFacesAction = QtGui.QAction(
+            QtGui.QIcon("res/group_faces.png"), "Group Similar Faces", self)
+        self._groupFacesAction.triggered.connect(self.onGroupFacesPressed)
+
+        self._exitAction = QtGui.QAction(
+            QtGui.QIcon("res/exit.png"), "Exit", self)
+        self._exitAction.triggered.connect(self.close)
+
+        self._toolbar = QtWidgets.QToolBar()
+        self._toolbar.setMovable(False)
+        self._toolbar.setFloatable(False)
+        self._toolbar.setIconSize(QtCore.QSize(48, 48))
+        self._toolbar.setToolButtonStyle(QtCore.Qt.ToolButtonStyle.ToolButtonTextBesideIcon)
+        self._toolbar.addAction(self._addImagesAction)
+        self._toolbar.addAction(self._exportImageAction)
+        self._toolbar.addAction(self._correctPerspectiveAction)
+        self._toolbar.addAction(self._deblurAction)
+        self._toolbar.addAction(self._groupFacesAction)
+        self._toolbar.setStyleSheet("""QToolBar QToolButton { width: 200px; }""")
+        self.addToolBar(QtCore.Qt.ToolBarArea.LeftToolBarArea, self._toolbar)
 
         splitter = QtWidgets.QSplitter(QtCore.Qt.Horizontal)
         self.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding))
@@ -46,14 +70,14 @@ class MainWindow(QtWidgets.QMainWindow):
         setSplitterStyle(splitter)
 
         self.image_grid = ImageGrid()
-        for image_path in self.get_test_image_paths():
+        for image_path in self.getTestImagePaths():
             try:
                 image = Image(image_path)
                 self.image_grid.addImage(image)
             except Exception as e:
                 print(f"Failed to load image {image_path}: {e}")
 
-        self.image_grid.selectionChanged.connect(self.on_image_grid_selection_changed)
+        self.image_grid.selectionChanged.connect(self.onImageGridSelectionChanged)
 
         self.inspector_panel = InspectorPanel()
         self.inspector_panel.setContentsMargins(0, 0, 0, 0)
@@ -69,13 +93,20 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self._menuBar = self.menuBar()
         self._fileMenu = self._menuBar.addMenu("&File")
-        self._fileMenu.addAction("&Import image", self.import_image)
-        self._fileMenu.addAction("&Export image", self.export_image)
-        self._fileMenu.addAction("&Exit", self.close)
+        self._fileMenu.addAction(self._addImagesAction)
+        self._fileMenu.addAction(self._exportImageAction)
+        self._fileMenu.addAction(self._exitAction)
+
+        self._editMenu = self._menuBar.addMenu("&Edit")
+        self._editMenu.addAction(self._correctPerspectiveAction)
+        self._editMenu.addAction(self._deblurAction)
+        self._editMenu.addAction(self._groupFacesAction)
 
         self._childWindows = []  # Only because GC closes the window when the reference is lost.
 
-    def get_test_image_paths(self):
+        self.onImageGridSelectionChanged()  # Refresh the UI for the first time.
+
+    def getTestImagePaths(self):
         max_image_count = 2000
         image_paths = [
             "test_images/icon.png",
@@ -92,22 +123,7 @@ class MainWindow(QtWidgets.QMainWindow):
         return image_paths[:max_image_count]
 
     @QtCore.Slot()
-    def open_cam_detect_demo(self) -> None:
-        video = cv2.VideoCapture(0)
-
-        while True:
-            _, frame = video.read()
-            faces = landmark(frame, upsample=1)
-            frame = draw_faces(frame, faces)
-            cv2.imshow("Caras", frame)
-            key = cv2.waitKey(1)
-            if key == ord("q"):
-                break
-
-        video.release()
-
-    @QtCore.Slot()
-    def on_image_grid_selection_changed(self) -> None:
+    def onImageGridSelectionChanged(self) -> None:
         selected_images = self.image_grid.selectedImages()
         self.inspector_panel.setSelectedImages(selected_images)
         count = len(selected_images)
@@ -118,15 +134,20 @@ class MainWindow(QtWidgets.QMainWindow):
         else:
             self.statusBar().showMessage("{} images selected".format(count))
 
+        self._exportImageAction.setEnabled(count > 0)
+        self._correctPerspectiveAction.setEnabled(count == 1)
+        self._deblurAction.setEnabled(count == 1)
+        self._groupFacesAction.setEnabled(count > 1 or count == 0)
+
     @QtCore.Slot()
-    def import_image(self) -> None:
+    def onAddImagesPressed(self) -> None:
         file_path = QtWidgets.QFileDialog.getOpenFileName(self, "Open File", "", "Images (*.png *.jpg *.jpeg)")[0]
         if file_path:
             image = Image(file_path)
             self.image_grid.addImage(image)
 
     @QtCore.Slot()
-    def export_image(self) -> None:
+    def onExportImagePressed(self) -> None:
         selected_images = self.image_grid.selectedImages()
         if len(selected_images) == 1:
             file_path = QtWidgets.QFileDialog.getSaveFileName(self, "Save File", "", "Images (*.png *.jpg *.jpeg)")[0]
@@ -134,7 +155,7 @@ class MainWindow(QtWidgets.QMainWindow):
                 selected_images[0].save(file_path)
 
     @QtCore.Slot()
-    def open_perspective_correct_window(self) -> None:
+    def onCorrectPerspectivePressed(self) -> None:
         selected = self.image_grid.selectedImages()
         if len(selected) == 1:
             window = PerspectiveWindow(selected[0])
@@ -142,9 +163,22 @@ class MainWindow(QtWidgets.QMainWindow):
             window.showMaximized()
 
     @QtCore.Slot()
-    def open_deblur_window(self) -> None:
+    def onDeblurPressed(self) -> None:
         selected = self.image_grid.selectedImages()
         if len(selected) == 1:
             window = DeblurWindow(selected[0])
             self._childWindows.append(window)
             window.showMaximized()
+
+    @QtCore.Slot()
+    def onGroupFacesPressed(self) -> None:
+        selected = self.image_grid.selectedImages()
+        if len(selected) == 1:
+            return
+
+        if len(selected) == 0:
+            selected = self.image_grid.images()
+
+        window = GroupFacesWindow(selected)
+        self._childWindows.append(window)
+        window.showMaximized()
