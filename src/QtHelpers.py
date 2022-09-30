3be5b4d2d3cad26e54ab80d5b28e551b195128b3
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2022-09-30 01:25:40 -0300

    Improve documentation for QtHelpers

--- src/QtHelpers.py
+++ src/QtHelpers.py
@@ -2,7 +2,13 @@ from PySide6 import QtCore, QtWidgets
 
 
 def setSplitterStyle(splitter: QtWidgets.QSplitter):
-    # The splitter is invisible due to a bug in Qt so we use an image instead.
+    """
+    Set the style of a splitter to a drag handle image.
+    This helper is required due to a bug in Qt where the splitter is invisible.
+
+    Args:
+        splitter (QSplitter): The splitter to set the style for.
+    """
     if splitter.orientation() == QtCore.Qt.Horizontal:
         splitter.setStyleSheet("QSplitter::handle { image: url(res/drag_handle_horizontal.png); }")
     else:
