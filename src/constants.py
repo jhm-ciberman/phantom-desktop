091f7428fccaf1fdd38918d3f3137e08c14264bd
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2022-10-20 23:22:17 -0300

    Bump version to v0.1.0

--- src/constants.py
+++ src/constants.py
@@ -2,7 +2,7 @@ from os import path
 import os
 
 
-app_version = "1.0.0"
+app_version = "0.1.0"  # Any semver is fine
 app_name = "Phantom Desktop"
 app_repo_url = "https://github.com/jhm-ciberman/phantom-desktop"
 app_docs_url = f"{app_repo_url}/wiki"
@@ -10,7 +10,7 @@ app_bugs_url = f"{app_repo_url}/issues/new"
 app_log_file = "phantom-desktop.log"
 
 # ModelsDownloader
-models_release_tag = "v0.0.0"
+models_release_tag = "v0.1.0"
 models_zip_filename = "models.zip"
 models_zip_url = f"{app_repo_url}/releases/download/{models_release_tag}/{models_zip_filename}"
 models_local_folder = path.join(os.getcwd(), "models")
